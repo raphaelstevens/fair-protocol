413b8407a7a67c63341a5b0cde9b4489a6f8f440
unknown <raphael.stevens@gmail.com>
2024-12-30 00:16:48 +0100

    streamlining chart commits - test 4

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,41 +4,29 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'global': 'globalThis',
-    'process': {
-      'env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    }
-  },
   build: {
     outDir: '../static/dist',
     emptyOutDir: true,
-    lib: {
-      entry: resolve(__dirname, 'src/main.jsx'),
-      name: 'FairProtocol',
-      formats: ['es'],
-      fileName: 'fair-protocol'
-    },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      input: resolve(__dirname, 'src/main.jsx'),
       output: {
+        entryFileNames: 'fair-protocol.js',
+        format: 'iife',
+        name: 'FairProtocol',
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM'
         }
-      }
-    }
+      },
+      external: ['react', 'react-dom']
+    },
+    cssCodeSplit: false,
+    // Assurez-vous que le CSS est extrait dans un fichier séparé
+    cssMinify: true
   },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    watch: {
-      usePolling: true,
     },
   }
 });
